beab87f6b656662a174ad09d699b553e7d8d9b32
Robert_Norwood <robnorwood91@gmail.com>
2020-12-31 16:57:33 -0800

    added authorization for reviews and campgrounds, as well as added icons to buttons

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,22 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const catchAsync = require('../utilities/catchAsync.js')
-const ExpressError = require('../utilities/ExpressError.js')
 const Campground = require('../models/campground.js')
-const {campgroundSchema} = require('../schemas.js')
-const {isLoggedIn} = require('../middleware')
+const {isLoggedIn, validateCampground, isAuthor} = require('../middleware')
 
-
-
-const validateCampground = (req, res, next) => {
-	const { error } = campgroundSchema.validate(req.body)
-	if(error) {
-		const msg = error.details.map(el => el.message).join(',')
-		throw new ExpressError(msg, 400)
-	} else {
-		next()
-	}
-}
 
 //INDEX ROUTE
 router.get('/', catchAsync(async (req, res) => {
@@ -40,7 +27,9 @@ router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, nex
 
 //SHOW ROUTE
 router.get('/:id', catchAsync(async (req, res) => {
-	const campground = await (await Campground.findById(req.params.id).populate('reviews').populate('author'))
+	// find the campground by it's id, then populate all of the reviews for that campground, then populate the author of each review.
+	//lastly populate the author of the entire campground
+	const campground = await (await Campground.findById(req.params.id).populate({path:'reviews', populate:{path:'author'}}).populate('author'))
 
 	if(!campground) {
 		req.flash('error', "This campground doesn't exist")
@@ -51,44 +40,28 @@ router.get('/:id', catchAsync(async (req, res) => {
 
 
 //EDIT ROUTE
-router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
 	const { id } = req.params
 	const campground = await Campground.findById(id)
 	if(!campground) {
 		req.flash('error', "This campground doesn't exist")
 		return res.redirect('/campgrounds')
 	}
-
-	if (!campground.author.equals(req.user._id)) {
-		req.flash("error", "You don't have permission to do that")
-		return res.redirect(`/campgrounds/${id}`)
-	}
-
 	res.render('campgrounds/edit', { campground })
 }))
 
 //UPDATE ROUTE
-router.put('/:id', isLoggedIn, validateCampground, catchAsync(async (req, res) => {
+router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(async (req, res) => {
 	const { id } = req.params
-	const campground = await Campground.findById(id)
-	if (!campground.author.equals(req.user._id)) {
-		req.flash("error", "You don't have permission to do that")
-		return res.redirect(`/campgrounds/${id}`)
-	}
-	const camp = await Campground.findByIdAndUpdate(req.params.id, {...req.body.campground})
+	const campground = await Campground.findByIdAndUpdate(req.params.id, {...req.body.campground})
 	req.flash('success', 'Successfully updated campground')
 	res.redirect(`/campgrounds/${id}`)
 }))
 
 //DELETE ROUTE
-router.delete('/:id', isLoggedIn, catchAsync(async (req, res) => {
+router.delete('/:id', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
 	//destructure the params
 	const {id} = req.params
-	const camp = await Campground.findById(id)
-	if (!campground.author.equals(req.user._id)) {
-		req.flash("error", "You don't have permission to do that")
-		return res.redirect(`/campgrounds/${id}`)
-	}
 	const campground = await Campground.findByIdAndDelete(id)
 	req.flash('success', 'Successfully deleted campground')
 	res.redirect('/campgrounds')
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,14 +1,10 @@
 const express = require('express')
 //merges all of the params from the campgrounds file into the review params in this file. So we have access to the campgrounds params.
 const router = express.Router({mergeParams: true})
-
-
 const Campground = require('../models/campground.js')
 const Review = require('../models/review')
 
-
-const { reviewSchema } = require('../schemas.js')
-
+const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware')
 
 const ExpressError = require('../utilities/ExpressError.js')
 const catchAsync = require('../utilities/catchAsync.js')
@@ -16,26 +12,18 @@ const catchAsync = require('../utilities/catchAsync.js')
 
 
 
-const validateReview = (req, res, next) => {
-	const { error } = reviewSchema.validate(req.body)
-	if(error) {
-		const msg = error.details.map(el => el.message).join(',')
-		throw new ExpressError(msg, 400)
-	} else {
-		next()
-	}
-}
-
-
 //REVIEW POST ROUTE
-router.post('/', validateReview, catchAsync (async(req, res) => {
+router.post('/', isLoggedIn, validateReview, catchAsync (async(req, res) => {
 	//finds the campground
 	const campground = await Campground.findById(req.params.id)
 	//creates the new review
 	const review = new Review(req.body.review)
+	//set the author of the review to the current user
+	review.author = req.user._id
 	//push the newly created review to the campground's reviews array
 	campground.reviews.push(review)
 	await review.save()
+	//since we're storing a reference of the review in the campground's reviews array, we save the campground here as well
 	await campground.save()
 	//Shows message that your review was successfully created
 	req.flash('success', 'Successfully created new review')
@@ -43,7 +31,7 @@ router.post('/', validateReview, catchAsync (async(req, res) => {
 }))
 
 //REVIEW DELETE ROUTE
-router.delete('/:reviewId', catchAsync (async(req, res) => {
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync (async(req, res) => {
 	//destructure the params
 	const { id, reviewId } = req.params
 	//we find the campground by the ID then
